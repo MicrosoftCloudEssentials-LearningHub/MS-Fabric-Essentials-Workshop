3418d6ad514d046e65781b97d3de729c5734c02e
Timna Brown <24630902+brown9804@users.noreply.github.com>
2025-01-29 14:20:41 -0600

    more outputs for refs

--- Terraform/src/outputs.tf
+++ Terraform/src/outputs.tf
@@ -1,3 +1,18 @@
+# Output the name of the resource group
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name  # The name of the resource group
+}
+
+# Output the name of the storage account
+output "storage_account_name" {
+  value = azurerm_storage_account.example.name  # The name of the storage account
+}
+
+# Output the name of the storage container
+output "storage_container_name" {
+  value = azurerm_storage_container.example.name  # The name of the storage container
+}
+
 # Output the name of the MSSQL Server
 output "sql_server_name" {
   value = azurerm_mssql_server.example.name  # The name of the SQL Server
@@ -10,5 +25,9 @@ output "sql_database_name" {
 
 # Output the name of the Microsoft Fabric Capacity
 output "fabric_capacity_name" {
-  value = azapi_resource.example.name  # The name of the Microsoft Fabric Capacity
+  value = azurerm_fabric_capacity.example.name
+}
+
+output "fabric_workspace_name" {
+  value = fabric_workspace.example.display_name
 }
