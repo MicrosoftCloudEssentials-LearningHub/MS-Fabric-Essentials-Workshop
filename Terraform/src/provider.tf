c55973839ba1462c247090a29c36a1fc380b8546
Timna Brown <24630902+brown9804@users.noreply.github.com>
2025-01-29 14:22:00 -0600

    provider for fabric

--- Terraform/src/provider.tf
+++ Terraform/src/provider.tf
@@ -1,13 +1,14 @@
 terraform {
+  required_version = ">= 1.8, < 2.0"
   # Specify the required providers and their versions
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"  # Source of the AzureRM provider
       version = "~> 4.16.0"          # Version of the AzureRM provider
     }
-    azapi = {
-      source  = "Azure/azapi"        # Source of the AzAPI provider
-      version = "~> 1.0.0"           # Version of the AzAPI provider
+    fabric = {
+      source  = "microsoft/fabric"
+      version = "0.1.0-beta.7"
     }
   }
 }
@@ -17,4 +18,4 @@ provider "azurerm" {
   subscription_id = var.subscription_id  # Add your subscription ID here
 }
 
-provider "azapi" {}
+provider "fabric" {}
